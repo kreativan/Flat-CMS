76a647e0a67a847baeae9a55cf63a1e37ce006a4
Ivan Milincic <lokomotivan@gmail.com>
2015-05-18 21:51:56 +0200

    menu override cleanup

--- Master/template/admin/menu.php
+++ Master/template/admin/menu.php
@@ -1,23 +1,23 @@
-<li><a href="index.php?p=home&f=blocks/@grid">Custom Widget</a></li>
+<li><a href="index.php?p=home&f=blocks/@tabs">Custom Widget</a></li>
 
 
 <!-- Protect files and folders -->
 <script>
 jQuery(function($) {
     
-    // @kreativan-tabs
-    $('a[href="index.php?p=home&f=blocks/@kreativan-tabs"]').addClass('ivm-protect');
+    // system - obavezno za svaki protect falj
     $('#ivm-admin-main .ivm-protect').parent().addClass('uk-hidden');
     $('#ivm-admin-main .folder-row.uk-hidden').parent().addClass('uk-hidden');
-    $('a[href="index.php?p=rename&d=blocks/@kreativan-tabs"]').addClass('uk-hidden');
     
-    // @kreativan-portfolio
-    $('a[href="index.php?p=home&f=media/@kreativan-portfolio"]').addClass('ivm-protect');
-    $('#ivm-admin-main .ivm-protect').parent().addClass('uk-hidden');
-    $('#ivm-admin-main .folder-row.uk-hidden').parent().addClass('uk-hidden');
-    $('a[href="index.php?p=rename&d=media/@kreativan-portfolio"]').addClass('uk-hidden');
-    $('a[href="index.php?p=upload&gallery=media/@kreativan-portfolio"] + .tagdiv input').val("{{kreativan-portfolio}}");
-    $('a[href="index.php?p=upload&gallery=media/@kreativan-portfolio"] + .tagdiv input+input').addClass("uk-hidden");
+    // @content block example
+    $('a[href="index.php?p=home&f=blocks/@tabs"]').addClass('ivm-protect');
+    $('a[href="index.php?p=rename&d=blocks/@tabs"]').addClass('uk-hidden');
+    
+    // @content media example
+    $('a[href="index.php?p=home&f=media/@portfolio"]').addClass('ivm-protect');
+    $('a[href="index.php?p=rename&d=media/@portfolio"]').addClass('uk-hidden');
+    $('a[href="index.php?p=upload&gallery=media/@portfolio"] + .tagdiv input').val("{{portfolio}}");
+    $('a[href="index.php?p=upload&gallery=media/@portfolio"] + .tagdiv input+input').addClass("uk-hidden");
 
  }); 
 </script>
