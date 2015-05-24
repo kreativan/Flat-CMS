b1418ae00f1dd28393a21052ffb1d6608f0426a2
Ivan Milincic <lokomotivan@gmail.com>
2015-05-24 16:33:57 +0200

    added dev-tools animation

--- Master/ivm/dev-tools/dev-tools.php
+++ Master/ivm/dev-tools/dev-tools.php
@@ -2,8 +2,8 @@
     <div><?php include("../ivm/dev-tools/pages.php");?></div>
 </div>
     
-<div id="ivm-dev-widgets" class="ivm-dev-tools uk-hidden uk-animation-slide-right uk-visible-large">   
-    <div><?php include("../ivm/dev-tools/widgets.php");?></div>
+<div id="ivm-dev-extensions" class="ivm-dev-tools uk-hidden uk-animation-slide-right uk-visible-large">   
+    <div><?php include("../ivm/dev-tools/extensions.php");?></div>
 </div>      
     
 <div id="ivm-dev-layout" class="ivm-dev-tools uk-hidden uk-animation-slide-right uk-visible-large">   
