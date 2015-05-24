61e8e5a01f11c9d79fa50552ec3c311ebb42e13e
Ivan Milincic <lokomotivan@gmail.com>
2015-05-25 00:07:07 +0200

    twitter plugin settings fix

--- Master/ivm/plugins/ivm-twitter.php
+++ Master/ivm/plugins/ivm-twitter.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
     // Settings
     $twitter_img = 'https://pbs.twimg.com/profile_images/463019833244659713/y2nJte5Q_400x400.jpeg';
     $twitter_img_size = '60px';
@@ -7,7 +7,7 @@
     $twitter_limit = '3';
     $show_time = 'true';
     $show_media = 'false';
-
+*/
 ?>
 
 <div id="tweecool" class="uk-clearfix"></div>
