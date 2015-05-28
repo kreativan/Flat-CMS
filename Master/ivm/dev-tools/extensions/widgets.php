16eef9447607d0497dc1c4c37242fb690b75c135
Ivan Milincic <lokomotivan@gmail.com>
2015-05-28 21:05:30 +0200

    auto add widget shortcode to devtools

--- Master/ivm/dev-tools/extensions/widgets.php
+++ Master/ivm/dev-tools/extensions/widgets.php
@@ -6,3 +6,17 @@
     <?php include("../template/admin/widgets.php");?> 
     
 </div><!-- ACC END -->
+
+<?php $folders = glob("../content/blocks/@widgets" . '/*' , GLOB_ONLYDIR); ?>
+<?php foreach($folders as $folder) : ?>
+    <?php 
+        $folder = basename($folder);
+        $folder_name = str_replace('@', '', $folder);
+    ?>
+
+        <div class="ivm-html-code-title"><?php echo $folder_name;?></div>
+        <div id="ivm-<?php echo $folder_name;?>-code" class="ivm-html-code" onclick="selectText('ivm-<?php echo $folder_name;?>-code')">
+            <?php echo '{{'.$folder_name.'}}'; ?>
+        </div>
+
+<?php endforeach;?>
